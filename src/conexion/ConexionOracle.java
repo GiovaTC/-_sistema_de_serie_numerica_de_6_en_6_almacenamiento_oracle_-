1a142f9c463c3c4f0c462524fc48d5ef4845a6e8
gtapiero <giovanny.tapiero@pi.edu.co>
2026-06-14 10:34:39 -0500

    commit - Serie .

--- src/conexion/ConexionOracle.java
+++ src/conexion/ConexionOracle.java
@@ -29,4 +29,4 @@ public class ConexionOracle {
             return null;
         }
     }
-}   
+}
--- src/modelo/Serie.java
+++ src/modelo/Serie.java
@@ -1,4 +1,58 @@
 package modelo;
 
 public class Serie {
-}
+
+    private int numeroInicial;
+    private int cantidadTerminos;
+    private String serieGenerada;
+    private int sumaTotal;
+
+    public Serie() {
+
+    }
+
+    public Serie(
+            int numeroInicial,
+            int cantidadTerminos,
+            String serieGenerada,
+            int sumaTotal) {
+
+        this.numeroInicial = numeroInicial;
+        this.cantidadTerminos = cantidadTerminos;
+        this.serieGenerada = serieGenerada;
+        this.sumaTotal = sumaTotal;
+
+    }
+
+    public int getNumeroInicial() {
+        return numeroInicial;
+    }
+
+    public void setNumeroInicial(int numeroInicial) {
+        this.numeroInicial = numeroInicial;
+    }
+
+    public int getCantidadTerminos() {
+        return cantidadTerminos;
+    }
+
+    public void setCantidadTerminos(int cantidadTerminos) {
+        this.cantidadTerminos = cantidadTerminos;
+    }
+
+    public String getSerieGenerada() {
+        return serieGenerada;
+    }
+
+    public void setSerieGenerada(String serieGenerada) {
+        this.serieGenerada = serieGenerada;
+    }
+
+    public int getSumaTotal() {
+        return sumaTotal;
+    }
+
+    public void setSumaTotal(int sumaTotal) {
+        this.sumaTotal = sumaTotal;
+    }
+}   
